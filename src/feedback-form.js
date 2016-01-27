f3baef82b3e1244edd5498435bc261e7d78e6f6a
i00die <engin.diri@gmail.com>
2016-01-27 10:57:41 +0100

    initial import

--- src/feedback-form.js
+++ src/feedback-form.js
@@ -11,17 +11,46 @@
     'use strict';
 
     angular.module('feedbackForm', []).directive('feedbackbutton', ['$window', function ($window) {
-        return {
-            restrict: 'E',
-            replace: true,
-            scope: {
-                buttonText: '@'
-            },
-            link: function (scope, element, attrs) {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    buttonText: '@',
+                    modalId:'@'
+                },
+                link: function (scope, element, attrs) {
 
-            },
-            template: "<div class=\"btn btn-primary feedback\" >{{buttonText}}</div>"
-        }
-            ;
-    }]);
+                },
+                template: "<div class=\"btn btn-primary feedback\" data-toggle=\"modal\" data-target=\"#{{modalId}}\">{{buttonText}}</div>"
+            }
+        }])
+
+        .directive('feedbackmodal', ['$window', function ($window) {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    id: '@'
+                },
+                link: function (scope, element, attrs) {
+
+                },
+                template: "<div id=\"{{id}}\" class=\"modal fade\" role=\"dialog\"> \
+                            <div class=\"modal-dialog\"> \
+                             <div class=\"modal-content\"> \
+                              <div class=\"modal-header\"> \
+                               <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button> \
+                               <h4 class=\"modal-title\">Modal Header</h4> \
+                              </div> \
+                              <div class=\"modal-body\"> \
+                               <p>Some text in the modal.</p> \
+                              </div> \
+                              <div class=\"modal-footer\"> \
+                               <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button> \
+                              </div> \
+                             </div> \
+                            </div> \
+                           </div>"
+            }
+        }]);
 })();
