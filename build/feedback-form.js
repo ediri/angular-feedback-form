fff13dc8f1dcfc97fc5f9688066fcdb85fbb9a99
i00die <engin.diri@gmail.com>
2016-01-27 09:35:33 +0100

    initial import

---
+++ build/feedback-form.js
@@ -0,0 +1,32 @@
+/*! 
+ * angular-feedback-form v0.0.1
+ * https://github.com/ediri/angular-feedback-form
+ * Copyright (c) 2016 ediri
+ * License: MIT
+ */
+/*
+ * angular-feedback-form
+ *
+ * Angular JS Feedback Button with a basic Feedback Form
+ *
+ * (c) 2015 ediri
+ * MIT Licenese
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('feedbackForm', []).directive('feedbackbutton', ['$window', function ($window) {
+        return {
+            restrict: 'E',
+            scope: {
+                buttonText: '@'
+            },
+            link: function (scope, element, attrs) {
+
+            },
+            template: "<div id=\"feedback\" >{{buttonText}}</div>"
+        }
+            ;
+    }]);
+})();
